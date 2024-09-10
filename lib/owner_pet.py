e7ec567ba10fc4d1b4bd803ca7ccbd9e9cbc67da
roy wahome <roy.njuguna@student.moringaschool.com>
2024-09-10 17:03:51 +0300

    the fainal commit

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,39 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"{pet_type} is not in the {Pet.PET_TYPES} list. Choose from {Pet.PET_TYPES}.")
+        
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = None
+
+        if owner is not None:
+            if not isinstance(owner, Owner):
+                raise Exception("The owner must be an instance of the Owner class.")
+            self.owner = owner
+            owner.add_pet(self)
+
+        Pet.all.append(self)
+        
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("The pet must be an instance of the Pet class.")
+        pet.owner = self
+        self._pets.append(pet)
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)  
+
+        
